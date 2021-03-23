6a43563ecb19f3fcdd99602533c2cb11b560cf4d
MilicTG <milic.tg@gmail.com>
2021-03-23 12:24:37 +0100

    Initial commit

---
+++ app/src/main/java/dev/milic/colors/MainActivity.kt
@@ -0,0 +1,17 @@
+package dev.milic.colors
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.milic.colors.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
